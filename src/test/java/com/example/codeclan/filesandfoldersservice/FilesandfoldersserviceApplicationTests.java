554b892ab02371fb3096dcabc9fef49294a2d52b
Ricky <rickycorrigan@icloud.com>
2018-10-09 18:20:10 +0100

    adds Repository's and empty tests

--- src/test/java/com/example/codeclan/filesandfoldersservice/FilesandfoldersserviceApplicationTests.java
+++ src/test/java/com/example/codeclan/filesandfoldersservice/FilesandfoldersserviceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.codeclan.filesandfoldersservice;
 
+import com.example.codeclan.filesandfoldersservice.Models.Folder;
+import com.example.codeclan.filesandfoldersservice.Repositorys.FileRepository;
+import com.example.codeclan.filesandfoldersservice.Repositorys.FolderRepository;
+import com.example.codeclan.filesandfoldersservice.Repositorys.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FilesandfoldersserviceApplicationTests {
 
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	FileRepository fileRepository;
+
+	@Autowired
+	FolderRepository folderRepository;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void createUserAndFolderThenSave() {
+
+		Folder folder1 = new Folder("folder");
+		folderRepository.save(folder1);
+
+	}
+
+	@Test
+	public void createUserAndFileThenSave() {
+	}
 }
